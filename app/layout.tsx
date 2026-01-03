f4911d0a3ba1b8398971c85b268fdae819b5feb5
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2026-01-04 01:17:17 +0330

    feat: make dark theme default

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="dark">
-      <body className="min-h-screen">
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body className="min-h-screen bg-navy">
         <Navbar />
         <main>{children}</main>
       </body>
